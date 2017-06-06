eef0367aaabef97c3d2b4fd2ee8b4fe823f528d1
blueNmad <gyu0212@gmail.com>
2017-06-06 20:07:01 +0900

    Fix compile error

--- src/main/java/lets/trasnfer/bot/CurrencyHandler.java
+++ src/main/java/lets/trasnfer/bot/CurrencyHandler.java
@@ -1,17 +1,14 @@
 package lets.trasnfer.bot;
 
-import javax.xml.ws.Response;
-
 /**
  * Created by spoon on 2017. 5. 28..
  */
 public class CurrencyHandler implements Handler {
+
 	@Override
-	public void handle(String message, Response response) {
+	public void handle(String message) {
 		// 네이버 환율 API를 이요해서
 		// 정보를 조회
-
-		response.setMesage("aa")
-
 	}
+
 }
