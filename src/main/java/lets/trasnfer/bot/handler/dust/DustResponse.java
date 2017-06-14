23f4dcf5cb9f4c875400d4405d943faa9196b6a6
shinkook.kim <shinkook.kim@lge.com>
2017-06-14 16:37:45 +0900

    add DustHandler and DustResponse

---
+++ src/main/java/lets/trasnfer/bot/handler/dust/DustResponse.java
@@ -0,0 +1,12 @@
+package lets.trasnfer.bot.handler.dust;
+
+import lombok.Data;
+
+/**
+ * Created by shinkook.kim on 2017-06-14.
+ */
+@Data
+public class DustResponse {
+    String grade;
+    int value;
+}
