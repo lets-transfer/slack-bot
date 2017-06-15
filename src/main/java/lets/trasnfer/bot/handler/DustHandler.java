925a1c637b70cf9b4664d061ba285c576fa0b178
shinkook.kim <shinkook.kim@lge.com>
2017-06-15 16:57:31 +0900

    modified DustHandler for add header

--- src/main/java/lets/trasnfer/bot/handler/DustHandler.java
+++ src/main/java/lets/trasnfer/bot/handler/DustHandler.java
@@ -5,11 +5,13 @@ import lets.trasnfer.bot.handler.dust.DustResponse;
 import lets.trasnfer.bot.websocket.vo.Message;
 import lets.trasnfer.bot.websocket.vo.ResponseMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.OkHttpClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
 import java.net.URI;
 
 /**
@@ -23,32 +25,42 @@ public class DustHandler implements MessageHandler {
     @Override
     public ResponseMessage handle(Message message) {
         ResponseMessage response = new ResponseMessage();
+        DustResponse dustResponse;
 
-        DustResponse dustResponse = connectServer(message);
+        try {
+            dustResponse = connectServer(message);
+        } catch (IOException e) {
 
-        response.setChannel(message.getChannel());
-        response.setType(message.getType());
-        response.setText(dustResponse.getGrade());
+        }
+
+//        response.setChannel(message.getChannel());
+//        response.setType(message.getType());
+//        response.setText(dustResponse.getGrade());
 
         return response;
     }
 
-    private DustResponse connectServer(Message message) {
+    private DustResponse connectServer(Message message) throws IOException {
         String[] split = message.getText().split(" ");
         log.info("split Test: {}, {}", split[0], split[1]);
 
         OkHttpClient client = new OkHttpClient();
         ClientHttpRequestFactory requestFactory = new OkHttpClientHttpRequestFactory(client);
+        // Dust server 에 접속
+//        connectDustServer();
+
+        //Header 추가
+        org.springframework.http.HttpHeaders headers = addHeaderForAPI();
+        Http
 
         //lat , lon 에 대한 정보를 가져와야 함
         //먼지 위치(lat , lon 위치 정보 필요)
-        //request:     http://apis.skplanetx.com/weather/airquality/current?lon=126&lat=37&version=1
-        //dusthandler: http://apis.skplanetx.com/weather/airquality/current?lon=126&lat=37&version=1
+        //가산 위도경도: 126.88382980000006 / 37.4758795
         URI uri = UriComponentsBuilder.newInstance().scheme("http")
                 .host(dustapiHost)
                 .path("weather/airquality/current")
-                .queryParam("lon", 126)
-                .queryParam("lat", 37)
+                .queryParam("lon", 126.88382980000006)
+                .queryParam("lat", 37.4758795)
                 .queryParam("version", 1)
                 .build()
                 .encode()
@@ -61,5 +73,11 @@ public class DustHandler implements MessageHandler {
 //        DustResponse dustResponse = new DustResponse();
 
         return dustResponse;
+    }
+
+    private org.springframework.http.HttpHeaders addHeaderForAPI() {
+        org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+        headers.add("appKey", "42a9fc24-5097-37c8-b05b-61919d08aac2");
+        return headers;
     }
 }
