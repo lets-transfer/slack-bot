a16adcdb85f2283290b56ed8acfa3b38bc499030
shinkook.kim <shinkook.kim@lge.com>
2017-06-16 20:44:43 +0900

    modify DustHandler for add header

--- src/main/java/lets/trasnfer/bot/handler/DustHandler.java
+++ src/main/java/lets/trasnfer/bot/handler/DustHandler.java
@@ -6,6 +6,7 @@ import lets.trasnfer.bot.websocket.vo.ResponseMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.OkHttpClientHttpRequestFactory;
@@ -14,6 +15,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Date;
 
 /**
  * Created by shinkook.kim on 2017-06-13.
@@ -22,7 +25,7 @@ import java.net.URI;
 public class DustHandler implements MessageHandler {
 
     String dustapiHost = "apis.skplanetx.com";
-    org.springframework.http.HttpEntity httpEntity;
+    org.springframework.http.HttpEntity<String> httpEntity;
 
     @Override
     public ResponseMessage handle(Message message) {
@@ -34,7 +37,6 @@ public class DustHandler implements MessageHandler {
         } catch (IOException e) {
 
         }
-
         return response;
     }
 
@@ -42,12 +44,8 @@ public class DustHandler implements MessageHandler {
         String[] split = message.getText().split(" ");
 
         log.info("split Test: {}, {}", split[0], split[1]);
-
         OkHttpClient client = new OkHttpClient();
         ClientHttpRequestFactory requestFactory = new OkHttpClientHttpRequestFactory(client);
-
-        //Header 추가 app key
-        httpEntity = addHeaderForAPI();
 
         //lat , lon 에 대한 정보를 가져와야 함
         //먼지 위치(lat , lon 위치 정보 필요)
@@ -63,21 +61,33 @@ public class DustHandler implements MessageHandler {
                 .toUri();
 
         log.info("uri print: " + uri.toString());
-
         RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+        //Header 추가 app key
+        httpEntity = addHeaderForHttpEntity();
+        log.info("Header info: " + httpEntity.getHeaders());
+        log.info("Body info: " + httpEntity.getBody());
+
+        //RestTemplate 로 HTTP request 전달
         //DustResponse dustResponse = restTemplate.exchange(uri, HttpMethod.GET,httpEntity, DustResponse.class);
         ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, String.class);
-
         log.info("response: " + responseEntity.toString());
         return responseEntity;
     }
 
-    private org.springframework.http.HttpEntity addHeaderForAPI() {
-
-        org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
-        headers.set("appKey", "42a9fc24-5097-37c8-b05b-61919d08aac2");
-        httpEntity = new org.springframework.http.HttpEntity(headers);
-
+    private org.springframework.http.HttpEntity addHeaderForHttpEntity() {
+        httpEntity = new org.springframework.http.HttpEntity<String>(makeHeader());
         return httpEntity;
+    }
+
+    private org.springframework.http.HttpHeaders makeHeader() {
+        org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+        headers.set("x-skpop-userId:", "ksbang39");
+        headers.set("Accept-Language:", "ko_KR");
+        headers.set("Accept:", "application/json");
+        headers.set("appKey:", "4fccb82f-c0db-3a21-ae15-3ad67988b6cc");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return headers;
     }
 }
