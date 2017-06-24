5beb5e482e06c77782ef085de8c3d86f76f78d0b
Spoon(김성호) <shkim84@coupang.com>
2017-06-24 11:44:53 +0900

    Preparing deployment

--- src/main/java/lets/trasnfer/bot/LetsTransferBot.java
+++ src/main/java/lets/trasnfer/bot/LetsTransferBot.java
@@ -3,18 +3,20 @@ package lets.trasnfer.bot;
 import lets.trasnfer.bot.handler.MessageDispatcher;
 import lets.trasnfer.bot.handler.coin.CoinCurrencyHandler;
 import lets.trasnfer.bot.handler.dust.DustHandler;
+import lets.trasnfer.bot.handler.hello.HelloHandler;
 import lets.trasnfer.bot.handler.movie.MovieInfoHandler;
 import lets.trasnfer.bot.websocket.SlackWebSocketConnector;
 
 import java.io.IOException;
 
-public class FirstBot {
+public class LetsTransferBot {
 	public static void main(String[] args) throws IOException {
 		MessageDispatcher dispatcher = new MessageDispatcher();
 		//dispatcher.addHandler("날씨", new WeatherHandler());
 		dispatcher.addHandler("먼지" , new DustHandler());
 		dispatcher.addHandler("코인", new CoinCurrencyHandler());
 		dispatcher.addHandler("영화", new MovieInfoHandler());
+		dispatcher.addHandler("안녕", new HelloHandler());
 
 		SlackWebSocketConnector connector = new SlackWebSocketConnector(dispatcher);
 		connector.initialize();
