a1d388d2dc74c23567ffcf7d13e07b454370969a
shinkook.kim <ksbang39@gmail.com>
2017-06-30 21:55:51 +0900

    add script for gradle - merge jar in IntellJ

---
+++ src/main/java/lets/trasnfer/bot/handler/dust/dustInfo/WebStatus.java
@@ -0,0 +1,12 @@
+package lets.trasnfer.bot.handler.dust.dustInfo;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Created by shinkook_mac on 2017. 6. 30..
+ */
+@Data
+public class WebStatus {
+	HttpStatus status;
+}
